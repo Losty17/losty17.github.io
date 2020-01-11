72e075d90804a17ffcd1741bdb14b33cfc623000
Losty17 <vinikappke@gmail.com>
2020-01-10 23:43:47 -0300

    header changed!

--- scripts/blurbg.js
+++ scripts/blurbg.js
@@ -1,11 +1,20 @@
 $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop()
     if(scroll < 300){
         $('.img-blur').css({
             transform: `translateY(${-scroll/7}px) scale(${1.1 - scroll/3000})`
         })
-        $('.img-parallax').css({
-            transform: `translateY(${+scroll/30}px) scale(${.8 + scroll/5000})`
+        $('.img-blur h1').css({
+            transform: `translateY(${+scroll/30}px) scale(${.8 + scroll/3000})`
+        })
+    }
+    if(scroll < 180){
+        $('.img-blur p').css({
+            transform: `rotate(${scroll}deg)`
+        })
+    }else{
+        $('.img-blur p').css({
+            transform: `rotate(180deg)`
         })
     }
 })
